ed09234ecc43a590bdbff55892029dfff46b4fe5
Daniel Stromberg <strombergundy@gmail.com>
2024-03-17 14:11:55 -0600

    Created base sensor test file

---
+++ SP_SensorTests/src/SP_SensorTests.cpp
@@ -0,0 +1,17 @@
+/* 
+ * Smart Plant Sensor Tests
+ * Author: Daniel Stromberg
+ * Date: 3/17/2024
+*/
+
+#include "Particle.h"
+SYSTEM_MODE(AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+void setup() {
+
+}
+
+void loop() {
+
+}
