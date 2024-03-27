edf88ebf5c139bb9d8eb02efe4e5059569562ef9
Daniel Stromberg <strombergundy@gmail.com>
2024-03-26 22:32:03 -0600

    Combined sensor and pump code

---
+++ PlantCareAndMonitoringSystem/src/PlantCareAndMonitoringSystem.cpp
@@ -0,0 +1,223 @@
+/* 
+ * Plant Care & Monitoring System
+ * Author: Daniel Stromberg
+ * Date: 3/26/2024
+*/
+
+#include "Particle.h"
+#include "math.h"
+#include <Air_Quality_Sensor.h>
+#include <Adafruit_MQTT/Adafruit_MQTT_SPARK.h>
+#include <Adafruit_MQTT/Adafruit_MQTT.h>
+#include "credentials.h"
+#include <Adafruit_BME280.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+//Sensor constants
+const int DUST_PIN = 10;
+const int AIR_QUALITY_PIN = A0;
+const int SAMPLE_TIME_MS = 30000;
+const int BME_ADDRESS = 0x76;
+const int SOIL_PIN = A1;
+
+//Display constants
+const int OLED_RESET =-1;
+const int OLED_ADDRESS = 0x3C;
+const int DEGREE_SYMBOL = 0xF8;
+
+const int PUMP_PIN = D19;
+const int PUMP_ON_TIME = 500;
+
+const int SENSOR_IN_WATER = 1744;     //when sensor is in pure pureWater
+const int SENSOR_IN_AIR = 3485;       //sensor value in air
+const int SENSOR_IN_WET_SOIL = 1770;   //sensor value in VERY wet sensor
+const int SENSOR_IN_DRY_SOIL = 3000;   //sensor value in dry soil
+const int MIN_WATERING_INTERVAL = 10000;    //Minimum time between watering.
+
+int currentMillis;
+int lastPumpTime = -9999;
+
+int duration;
+int totalLowTime = 0;
+int lastLowTime = 0;
+int lastSampleTime;
+float ratio = 0;
+float concentration = 0;
+String warningText;
+
+bool status;
+float tempC;
+float tempF;
+float humidity;
+int lastDisplayTime = 0;
+
+AirQualitySensor sensor(AIR_QUALITY_PIN);
+TCPClient TheClient;
+Adafruit_MQTT_SPARK mqtt(&TheClient, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+Adafruit_MQTT_Publish dustPub = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/plantinfo.dustsensor");
+Adafruit_MQTT_Publish airQualityPub = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/plantinfo.airquality");
+Adafruit_MQTT_Publish airQualityText = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/airqualitywarningtext");
+
+Adafruit_SSD1306 display(OLED_RESET);
+Adafruit_BME280 bme;
+
+
+SYSTEM_MODE(SEMI_AUTOMATIC);
+SYSTEM_THREAD(ENABLED);
+
+//functions
+void updateDisplays();
+float tempCtoF(float c);
+void MQTT_connect();
+bool MQTT_ping();
+
+void setup() {
+    Serial.begin(9600);
+    waitFor(Serial.isConnected, 10000);
+
+    pinMode(PUMP_PIN, OUTPUT);
+    pinMode(SOIL_PIN, INPUT);
+
+    status = bme.begin(BME_ADDRESS);
+    if (status == false){
+      Serial.printf("BME Failed to start at address 0x%02X", BME_ADDRESS);
+    }
+
+    display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.display();
+
+  pinMode(DUST_PIN, INPUT);
+  lastSampleTime = millis();
+}
+
+void loop() {
+    currentMillis = millis();
+    int quality = sensor.slope();
+    int timeSinceSample = currentMillis-lastSampleTime;
+    int timeSinceDisplay = currentMillis - lastDisplayTime;
+    int moistureLevel = analogRead(SOIL_PIN);
+    int timeSinceLastPump = currentMillis - lastPumpTime;
+
+    if (timeSinceDisplay > 10000){
+        tempF = tempCtoF(bme.readTemperature());
+        humidity = bme.readHumidity();
+        updateDisplays();
+        lastDisplayTime = millis();
+    }
+
+    duration = pulseIn(DUST_PIN, LOW);
+    totalLowTime = totalLowTime+duration;
+
+    if(timeSinceSample > SAMPLE_TIME_MS){
+        if(totalLowTime == 0){
+        totalLowTime = lastLowTime;
+        } else{
+        lastLowTime = totalLowTime;
+        }
+
+        ratio = totalLowTime/(SAMPLE_TIME_MS*10.0);
+        concentration = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62;
+        Serial.printf("Dust Concentration: %0.2f\n", concentration);
+
+        totalLowTime = 0;
+        lastSampleTime = millis();
+
+        if(quality == 0){
+            Serial.printf("Extreme VOCs!\n");
+            warningText = "Extreme VOCs!";
+        }else if(quality == 1){
+            Serial.printf("High VOCs\n");
+            warningText = "High VOCs.";
+        } else if (quality == 2){
+            Serial.printf("Low VOCs\n");
+            warningText = "Low VOCs";
+        } else if(quality == 3){
+            Serial.printf("Clean Air!\n");
+            warningText = "Fresh Air!";
+        }
+        Serial.printf("\n");
+        if(mqtt.Update()){
+            dustPub.publish(concentration);
+            airQualityPub.publish(quality);
+            airQualityText.publish(warningText);
+        }
+    }
+
+    //PUMP RELAY IS VERY SLOW! NEED TO BE FIXED.
+    if(currentMillis %5000 <= 5){
+        Serial.printf("Moisture Level: %i\n", moistureLevel);
+        Serial.printf("TimeSincePump: %i\n\n", timeSinceLastPump);
+    }
+
+    if(timeSinceLastPump > PUMP_ON_TIME){
+        digitalWrite(PUMP_PIN, LOW);
+    }
+
+    if(moistureLevel >3000 && timeSinceLastPump > MIN_WATERING_INTERVAL){
+        digitalWrite(PUMP_PIN, HIGH);
+        lastPumpTime = currentMillis;
+    }
+
+    MQTT_connect();
+    MQTT_ping();
+
+}
+
+//Convert sensor temperature from celcius to Fahrenheit
+float tempCtoF(float c){
+    return ((c*9/5)+32);
+}
+
+
+//Update all local displays at once
+void updateDisplays(){
+    // Serial.printf("Temperature: %0.1fF\n\n", tempF);
+    display.clearDisplay();
+    display.setCursor(0,0);
+    display.printf("Temp: %0.1f%cF\n\n", tempF, DEGREE_SYMBOL);
+    display.printf("Humidity: %0.1f%%", humidity);
+    display.display();
+}
+
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+    int8_t ret;
+
+    // Return if already connected.
+    if (mqtt.connected()) {
+    return;
+    }
+
+    Serial.print("Connecting to MQTT... ");
+
+    while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+        Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+        Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+        mqtt.disconnect();
+        delay(5000);  // wait 5 seconds and try again
+    }
+    Serial.printf("MQTT Connected!\n");
+}
+
+//Keeps the connection open to Adafruit
+bool MQTT_ping() {
+    static unsigned int last;
+    bool pingStatus;
+
+    if ((millis()-last)>120000) {
+        Serial.printf("Pinging MQTT \n");
+        pingStatus = mqtt.ping();
+        if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+        }
+        last = millis();
+    }
+    return pingStatus;
+}
